1ad29ad0eb1667a2e984271503b4754fceb3d7e0
Raghu Betina <raghu@firstdraft.com>
2019-06-11 04:49:29 +0000

    Generate restaurants resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # Routes for the Restaurant resource:
+
+  # CREATE
+  get("/restaurants/new", { :controller => "restaurants", :action => "new_form" })
+  post("/create_restaurant", { :controller => "restaurants", :action => "create_row" })
+
+  # READ
+  get("/restaurants", { :controller => "restaurants", :action => "index" })
+  get("/restaurants/:id_to_display", { :controller => "restaurants", :action => "show" })
+
+  # UPDATE
+  get("/restaurants/:prefill_with_id/edit", { :controller => "restaurants", :action => "edit_form" })
+  post("/update_restaurant/:id_to_modify", { :controller => "restaurants", :action => "update_row" })
+
+  # DELETE
+  get("/delete_restaurant/:id_to_remove", { :controller => "restaurants", :action => "destroy_row" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
